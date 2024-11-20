2e2a9b880e33474ed0ac7ef0cc5394bfe3468c84
shubhampawar4841 <shubhampawar4036@gmail.com>
2024-11-20 10:39:41 +0530

    configuration

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const cors=require('cors');
+const cookieParser = require('cookie-parser');
 
-app.use(express.json());
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
 
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"));
+app.use(cookieParser)
 app.get('/', (req, res) => {
     res.send('Server is running!');
 });
