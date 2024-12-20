3d4c6afdf5e87b9fa313ca6f208ce1c5881f24a8
Shubham Pawar <114252697+shubhampawar4841@users.noreply.github.com>
2024-12-20 23:50:42 +0530

    Update ApiResponse.js

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -6,5 +6,13 @@ class ApiResponse {
         this.success = statusCode < 400
     }
 }
+class apilog {
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
 
-export { ApiResponse }
+export { ApiResponse , apilog }
