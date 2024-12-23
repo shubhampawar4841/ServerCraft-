437be5a36ce2b747414a54bc00e533f288a7746c
Shubham Pawar <114252697+shubhampawar4841@users.noreply.github.com>
2024-12-23 21:40:53 +0530

    Update multer.middleware.js

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -13,16 +13,3 @@ const storage = multer.diskStorage({
 export const upload = multer({ 
     storage, 
 })
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/temp")
-    },
-    filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
-    }
-  })
-  
-export const upload = multer({ 
-    storage, 
-})
